71b49dcd446375b8fabf6103642c8c0d141ded4e
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2025-08-26 10:58:09 +0700

    done testing alpinejs

--- resources/views/testing/tailwind.blade.php
+++ resources/views/testing/tailwind.blade.php
@@ -7,17 +7,17 @@
     <title>Document</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body>
+<body x-data="{ diclick() { console.log('kenek') }, current_time: '{{ now() }}' }">
     <div class="d-card d-w-96 bg-base-100 d-shadow-xl">
         <figure>
-            
         </figure>
         <div class="d-card-body">
             <h2 class="d-card-title">My Project!</h2>
             <p>This is a description of my awesome new project built with Laravel, Tailwind, and DaisyUI.</p>
             <div class="d-card-actions justify-end">
-                <button class="d-btn d-btn-primary">Learn More</button>
+                <button class="d-btn d-btn-primary" x-on:click="diclick">Learn More</button>
             </div>
+            <h3 x-text="current_time"></h3>
         </div>
     </div>
 </body>
