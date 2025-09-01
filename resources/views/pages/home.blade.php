47c16e480332c74b6496c13ec777da6e44b3e788
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2025-09-01 14:29:32 +0700

    update header

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -40,42 +40,38 @@ $tPath = app()->environment('local') ? '' : '';
     <script>
         const csrfToken = "{{ csrf_token() }}";
     </script>
-    {{-- <header class="fixed top-0 left-0 w-full" style="z-index: 9999; height: 70px;">
-        <div class="relative left-1/2 -translate-x-1/2 d-navbar d-bg-base-100 d-shadow-sm py-3 h-full" style="width: 95%">
-            <div class="d-navbar-start">
-                <div class="d-dropdown">
-                    <div tabindex="0" role="button" class="d-btn d-btn-ghost lg:d-hidden">
-                    </div>
-                    <ul tabindex="0" class="d-menu d-menu-sm d-dropdown-content d-bg-base-100 d-rounded-box d-z-1 d-mt-3 d-w-52 d-p-2 d-shadow text-white"">
-                        <li><a href="/">Events</a></li>
-                        <li><a href="/">ddd</a></li>
-                        <li><a href="/">Item 3</a></li>
-                        <li><a href="/" >Contact</a></li>
-                    </ul>
-                </div>
-                <a class=""><img src="{{ asset($tPath.'assets/icon/logowhite.png') }}" alt="" class=""></img></a>
+    <nav class="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="{{ asset($tPath.'assets/icon/logowhite.png') }}" alt="" class="Uni Events Logo"></img>
+            </a>
+            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center">Login</button>
+                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
             </div>
-            <div class="d-navbar-center d-hidden lg:d-flex text-white"">
-                <ul class="d-menu d-menu-horizontal d-px-1">
-                    <li><a href="/" >Events</a></li>
-                    <li><a href="/" >About</a></li>
-                    <li><a href="/" >Blog</a></li>
-                    <li><a href="/" >Contact</a></li>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+                    <li>
+                        <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Services</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Contact</a>
+                    </li>
                 </ul>
             </div>
-            <div class="">
-                <a href="/login" class="rounded-xl"><span>Login</span></a>
-            </div>
         </div>
-    </header> --}}
-    <header class="fixed top-0 left-0 w-full" style="z-index: 9999; height: 70px;">
-        <div class="dropdown-menu">
-            <a class="/"><img src="{{ asset($tPath.'assets/icon/logowhite.png') }}" alt="" class=""></img></a>
-        </div>
-        <div>
-            <div></div>
-        </div>
-    </header>
+    </nav>
     <section class="relative h-screen">
         <div id="bg-hero">
             <div class="absolute top-0 left-0 w-full h-full"></div>
